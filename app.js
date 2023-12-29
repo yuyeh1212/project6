528ac49b1c31f7e9909accfa5d1b6cd4bd2e0977
yuyeh <herry861212@gmail.com>
2023-12-29 23:01:59 +0800

    彈跳球設置

--- app.js
+++ app.js
@@ -2,9 +2,35 @@ const c = document.getElementById("myCanvas");
 const canvasHeight = c.height;
 const canvasWidth = c.width;
 const ctx = c.getContext("2d");
+let circle_x = 160;
+let circle_y = 60;
+let radius = 20;
+let xSpeed = 20;
+let ySpeed = 20;
 
 function drawCircle() {
-  console.log("畫圓...");
+  //確認球是否打到邊界
+  if (circle_x >= canvasWidth - radius || circle_x <= radius) {
+    xSpeed *= -1;
+  }
+  if (circle_y >= canvasHeight - radius || circle_y <= radius) {
+    ySpeed *= -1;
+  }
+
+  //更動球的座標
+  circle_x += xSpeed;
+  circle_y += ySpeed;
+
+  //畫出黑色背景
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  //劃出圓球
+  ctx.beginPath();
+  ctx.arc(circle_x, circle_y, radius, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fillStyle = "skyblue";
+  ctx.fill();
 }
 
 let game = setInterval(drawCircle, 25);
